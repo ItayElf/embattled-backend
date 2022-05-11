2f2441a086cc8b434d61c376561fde59127bdb25
ItayElf <thh123321@gmail.com>
2022-05-11 23:51:08 +0300

    attack action

--- game/unit.py
+++ game/unit.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import math
+import random
 from dataclasses import dataclass
 
 from game.attack_arguments import AttackArguments
@@ -37,7 +38,7 @@ class Unit:
                    unit_data.hitpoints, unit_data.armor, unit_data.morale, unit_data.morale, unit_data.defense,
                    unit_data.melee_attack, unit_data.melee_damage, unit_data.charge_bonus, unit_data.ammunition,
                    unit_data.ammunition, unit_data.range, unit_data.ranged_attack, unit_data.ranged_damage,
-                   unit_data.attributes)
+                   [Attribute.from_model(a) for a in unit_data.attributes])
 
     @property
     def is_ranged(self):
@@ -67,3 +68,22 @@ class Unit:
         damage = self.ranged_damage if args.ranged else self.melee_damage
         total = ((damage * damage) / (2 * other.armor)) * hits * self.get_modifiers_for_attack(other, args)
         return round(total, 2)
+
+    def attack(self, other: Unit, args: AttackArguments):
+        """Resolve an attack of this unit on 'other' and returns the damage done and casualties"""
+        damage = self.calc_damage(other, args) * random.uniform(0.85, 1)
+        starting_size = other.unit_size
+        pool = other.unit_size * other.hitpoints
+        pool = max((0, pool - damage))
+        other.unit_size = math.ceil(pool / other.unit_size)
+        casualties = starting_size - other.unit_size
+
+        if args.ranged:
+            self.ammunition -= 1
+
+        if other.unit_size:
+            morale_modifier = 1.5 if args.flank else 1
+            ratio = casualties / starting_size + 0.5
+            other.morale -= casualties * ratio * morale_modifier
+            other.morale = max(math.ceil(other.morale), 0)
+        return damage, casualties
