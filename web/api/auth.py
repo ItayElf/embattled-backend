8a4a52a5976f0d7c097e860199cb38bd076c3c88
ItayElf <thh123321@gmail.com>
2022-05-14 23:47:52 +0300

    profile picture

--- web/api/auth.py
+++ web/api/auth.py
@@ -1,14 +1,18 @@
 import hashlib
+import io
+import os.path
 import random
 from string import printable
 
 import sqlalchemy.exc
-from flask import request, jsonify
+from flask import request, jsonify, send_file
 from flask_jwt_extended import create_refresh_token, create_access_token, jwt_required, get_jwt_identity
 from web import User
 from web.base import app, db
 
 SALT_SIZE = 32
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "blank_profile.webp"), "rb") as f:
+    _blank_profile = f.read()
 
 
 @app.route("/api/auth/login", methods=["POST"])
@@ -62,3 +66,25 @@ def auth_current_user():
     if not user:
         return "No user was found with this token", 404
     return jsonify(user.serialized)
+
+
+@app.route("/api/auth/profile/<int:idx>")
+def auth_profile_pic(idx):
+    user = User.query.filter_by(id=idx).first()
+    if not user:
+        return "", 404
+    if user.profile_pic:
+        return send_file(io.BytesIO(user.profile_pic), mimetype="image/webp")
+    else:
+        return send_file(io.BytesIO(_blank_profile), mimetype="image/webp")
+
+
+@app.route("/api/auth/profile/<name>")
+def auth_profile_pic_name(name):
+    user = User.query.filter_by(name=name).first()
+    if not user:
+        return "", 404
+    if user.profile_pic:
+        return send_file(io.BytesIO(user.profile_pic), mimetype="image/webp")
+    else:
+        return send_file(io.BytesIO(_blank_profile), mimetype="image/webp")
