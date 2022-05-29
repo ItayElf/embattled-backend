a33b02f3b371dde38dafc15c979ef0611e159b02
ItayElf <thh123321@gmail.com>
2022-05-29 09:53:42 +0300

    damage calc endpoint

--- web/api/units.py
+++ web/api/units.py
@@ -1,5 +1,9 @@
-from flask import jsonify
+import math
 
+from flask import jsonify, request
+
+from game.attack_arguments import AttackArguments
+from game.unit import Unit
 from web import UnitData
 from web.base import app
 
@@ -19,8 +23,46 @@ def units_unit_by_id(idx):
 
 
 @app.route("/api/unit/<name>")
-def unit_by_name(name):
+def units_unit_by_name(name):
     unit = UnitData.query.filter(UnitData.name.ilike(name)).first()
     if not unit:
         return f"No unit with name {name} was found", 404
     return jsonify(unit.serialized)
+
+
+@app.route("/api/damage_calc")
+def units_damage_calc():
+    attacker_name = request.args.get("attacker", None)
+    defender_name = request.args.get("defender", None)
+    if not attacker_name or not defender_name:
+        return "Missing Parameters", 400
+    attacker = UnitData.query.filter_by(name=attacker_name).first()
+    defender = UnitData.query.filter_by(name=defender_name).first()
+    if not attacker:
+        return f"No unit named {attacker_name}", 404
+    if not defender:
+        return f"No unit named {defender_name}", 404
+    attacker_size = int(request.args.get("attacker_size", str(attacker.unit_size)))
+    defender_size = int(request.args.get("defender_size", str(defender.unit_size)))
+    ranged = bool(int(request.args.get("ranged", "0")))
+    flank = bool(int(request.args.get("flank", "0")))
+    charge = bool(int(request.args.get("charge", "0")))
+    advantage = int(request.args.get("advantage", "0"))
+    args = AttackArguments(ranged, flank, charge, advantage)
+    attacker = Unit.from_data(attacker)
+    attacker.unit_size = attacker_size
+    defender.unit_size = defender_size
+    defender = Unit.from_data(defender)
+    d = attacker.calc_damage(defender, args)
+    return jsonify(
+        {
+            "max_damage": d,
+            "min_damage": d * 0.85,
+            "avg_damage": d * 0.925,
+            "max_casualties": int(d / defender.hitpoints),
+            "min_casualties": int(d * 0.85 / defender.hitpoints),
+            "avg_casualties": int(d * 0.925 / defender.hitpoints),
+            "min_turns_to_kill": math.ceil(defender.unit_size / int(d / defender.hitpoints)),
+            "max_turns_to_kill": math.ceil(defender.unit_size / int(d * 0.85 / defender.hitpoints))
+        }
+    )
